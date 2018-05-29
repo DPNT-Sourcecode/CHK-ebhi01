39fd862f76a34f218567878429cb7754fad850fc
sbx_user1059 <sbx_user1059@ip-10-12-114-174.eu-west-2.compute.internal>
2018-05-29 12:27:06 +0000

    Tue May 29 12:27:06 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -3,43 +3,50 @@ package befaster.solutions.CHK;
 import java.util.HashMap;
 
 public class CheckoutSolution {
-	public Integer checkout(String skus) {
-
+	
+	final HashMap<String, Integer> products = new HashMap<String, Integer>(0);
+	final HashMap<String, Integer> productCount = new HashMap<String, Integer>(0);
+	
+	public Integer checkout(String skus)
+	{
+		// clear down maps.  Actual population of the maps would normally
+		// be done elsewhere, not in the checkout() method.
+		this.products.clear();
+		this.productCount.clear();
+		
+		// sku prices
+		this.products.put("A", new Integer(50));
+		this.products.put("B", new Integer(30));
+		this.products.put("C", new Integer(20));
+		this.products.put("D", new Integer(15));
+		this.products.put("E", new Integer(40));
+		this.products.put("F", new Integer(10));
+		this.products.put("G", new Integer(20));
+		this.products.put("H", new Integer(10));
+		this.products.put("I", new Integer(35));
+		this.products.put("J", new Integer(60));
+		this.products.put("K", new Integer(80));
+		this.products.put("L", new Integer(90));
+		this.products.put("M", new Integer(15));
+		this.products.put("N", new Integer(40));
+		this.products.put("O", new Integer(10));
+		this.products.put("P", new Integer(50));
+		this.products.put("Q", new Integer(30));
+		this.products.put("R", new Integer(50));
+		this.products.put("S", new Integer(30));
+		this.products.put("T", new Integer(20));
+		this.products.put("U", new Integer(40));
+		this.products.put("V", new Integer(50));
+		this.products.put("W", new Integer(20));
+		this.products.put("X", new Integer(90));
+		this.products.put("Y", new Integer(10));
+		this.products.put("Z", new Integer(50));
+		
 		// assumes a basic string input.
 		int value = 0;
-		if (skus.length() > 0) {
-			String[] tokens = skus.split("(?!^)");
-
-			final HashMap<String, Integer> products = new HashMap<String, Integer>(0);
-			// sku prices
-			products.put("A", new Integer(50));
-			products.put("B", new Integer(30));
-			products.put("C", new Integer(20));
-			products.put("D", new Integer(15));
-			products.put("E", new Integer(40));
-			products.put("F", new Integer(10));
-			products.put("G", new Integer(20));
-			products.put("H", new Integer(10));
-			products.put("I", new Integer(35));
-			products.put("J", new Integer(60));
-			products.put("K", new Integer(80));
-			products.put("L", new Integer(90));
-			products.put("M", new Integer(15));
-			products.put("N", new Integer(40));
-			products.put("O", new Integer(10));
-			products.put("P", new Integer(50));
-			products.put("Q", new Integer(30));
-			products.put("R", new Integer(50));
-			products.put("S", new Integer(30));
-			products.put("T", new Integer(20));
-			products.put("U", new Integer(40));
-			products.put("V", new Integer(50));
-			products.put("W", new Integer(20));
-			products.put("X", new Integer(90));
-			products.put("Y", new Integer(10));
-			products.put("Z", new Integer(50));
-
-			final HashMap<String, Integer> productCount = new HashMap<String, Integer>(0);
+		if (skus.length() > 0)
+		{
+			String[] tokens = skus.split("(?!^)"); // split into individual characters.
 
 			// check each string token
 			for (String str : tokens)
@@ -74,26 +81,38 @@ public class CheckoutSolution {
 				int bDiscount = 15; // (2 * 30) - 45
 				int eDiscount = 30; // one B.
 				int fDiscount = 10; // one F.
+				int h5Discount = 5; // (5 * 10) - 45.
+				int h10Discount = 20; // (10 * 10) - 80.
 				
 				// work out how many deals have been claimed.
 				int a5Deals = aCount / 5;
 				int a3Deals = (aCount % 5) / 3;
 				int eDealsPotential = eCount / 2;
 				int eDeals = (bCount >= eDealsPotential) ? eDealsPotential : 0;
-				int h10Deals = aCount / 10;
-				int h5Deals = (aCount % 10) / 5;
+				int h10Deals = hCount / 10;
+				int h5Deals = (hCount % 10) / 5;
 
 				int bDeals = (bCount - eDeals) / 2;
 				int fDeals = (fCount) / 3;
 
 				// update value by removing the discounts for the deals.
-				value = value - (a5Deals * a5Discount) // better deal for customer.
-						- (a3Deals * a3Discount) // pick up any remaining 3 piece deals.
-						- (bDeals * bDiscount) // apply b discount.
-						- (eDeals * eDiscount) // apply e discount.
-						- (fDeals * fDiscount); // apply f discount.
+				value = value
+						- (a5Deals * a5Discount) 	// better A deal for customer.
+						- (a3Deals * a3Discount) 	// pick up any remaining 3 piece deals.
+						- (bDeals * bDiscount)   	// apply b discount.
+						- (eDeals * eDiscount)   	// apply e discount.
+						- (fDeals * fDiscount)   	// apply f discount.
+						- (h10Deals * h10Discount) 	// better H deal for customer.
+						- (h5Deals * h5Discount)   	// pick up any remaining 5 piece deals.
 			}
 		}
 		return value;
 	}
+	
+	final int calculatePrice(String product, int unitPrice, int unitCount, int discount)
+	{
+		int aCount = productCount.get(product).intValue();
+		
+	}
+	
 }
